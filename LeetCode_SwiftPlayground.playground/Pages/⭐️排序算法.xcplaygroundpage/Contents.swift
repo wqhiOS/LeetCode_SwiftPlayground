337e7c19b757679c25f5cff47cad11ca1a57f138
wuqihan <wuqihan123@163.com>
2019-08-14 14:41:01 +0800

    insertion sort

--- LeetCode_SwiftPlayground.playground/Pages/⭐️排序算法.xcplaygroundpage/Contents.swift
+++ LeetCode_SwiftPlayground.playground/Pages/⭐️排序算法.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 
-var array = [4,5,6,3,2,1]
+var array = [4,5,6,3,2,1,7]
 
 //bubble sort
 func bubbleSort(_ array: inout [Int]) {
@@ -18,6 +18,45 @@ func bubbleSort(_ array: inout [Int]) {
     }
     
 }
+//test
+//bubbleSort(&array)
+
+//insertion sort
+func insertionSort(_ array: inout [Int]) {
+    for i in 1..<array.count {
+        let value = array[i]
+        for j in stride(from: i-1, to: -1, by: -1) {
+            if array[j] > value {
+
+                array[j+1] = array[j]
+            }else {
+                break
+            }
+            
+            if j == 0 {
+                array[0] = value
+            }
+        }
+
+    }
+}
+func insertionSort2(_ arr:[Int]) -> [Int] {
+    if arr.count<=1 {return arr}
+    var sortedArr = arr
+    var tempInt:Int
+    for i in 0..<arr.count {
+        tempInt = sortedArr[i]
+        for j in stride(from: i, to: -1, by: -1){
+            if tempInt < sortedArr[j]{
+                sortedArr.remove(at: j + 1)
+                sortedArr.insert(tempInt, at: j)
+            }
+        }
+    }
+    return sortedArr
+}
+//test
+insertionSort(&array)
 
 
 
