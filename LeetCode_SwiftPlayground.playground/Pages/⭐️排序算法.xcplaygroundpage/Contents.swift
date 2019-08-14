482b3603fc67416dfb88e4fbc1ade8f45b58e03b
wuqihan <wuqihan123@163.com>
2019-08-14 09:47:46 +0800

    冒泡排序

--- LeetCode_SwiftPlayground.playground/Pages/⭐️排序算法.xcplaygroundpage/Contents.swift
+++ LeetCode_SwiftPlayground.playground/Pages/⭐️排序算法.xcplaygroundpage/Contents.swift
@@ -3,8 +3,19 @@
 import Foundation
 
 var array = [4,5,6,3,2,1]
-for (index,value) in array.enumerated() {
-    
+
+//冒泡排序
+func bubbleSort(_ array: inout [Int]) {
+    for i in 0..<array.count-1 {
+        for j in 0..<array.count-1-i {
+            if array[j] > array[j+1] {
+                (array[j],array[j+1]) = (array[j+1],array[j])
+            }
+        }
+    }
 }
+
+
+
 
 //: [Next](@next)
