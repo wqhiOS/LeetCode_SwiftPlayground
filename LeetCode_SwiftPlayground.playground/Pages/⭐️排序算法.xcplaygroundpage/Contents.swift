9861eb8794e1946de0524834f841f567b614939a
wuqihan <wuqihan123@163.com>
2019-08-14 09:57:13 +0800

    bubble sort

--- LeetCode_SwiftPlayground.playground/Pages/⭐️排序算法.xcplaygroundpage/Contents.swift
+++ LeetCode_SwiftPlayground.playground/Pages/⭐️排序算法.xcplaygroundpage/Contents.swift
@@ -4,15 +4,19 @@ import Foundation
 
 var array = [4,5,6,3,2,1]
 
-//冒泡排序
+//bubble sort
 func bubbleSort(_ array: inout [Int]) {
+    var flag = false
     for i in 0..<array.count-1 {
         for j in 0..<array.count-1-i {
             if array[j] > array[j+1] {
                 (array[j],array[j+1]) = (array[j+1],array[j])
+                flag = true
             }
         }
+        if !flag {break}
     }
+    
 }
 
 
